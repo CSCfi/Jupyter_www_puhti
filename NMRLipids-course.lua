a74eb4fa0051f7013eb17f389c80cac551ed2129
Laxmana Yetukuri <yetukuri@puhti-login2.bullx>
2022-06-09 08:55:00 +0300

    course instruxtions update

---
+++ NMRLipids-course.lua
@@ -0,0 +1,11 @@
+-- Jupyter
+prepend_path("PATH", "/projappl/project_xxxxx/NMRLipids/bin")
+
+setenv("_COURSE_BASE_NAME", "NMRLipids")
+setenv("_COURSE_NOTEBOOK", "DatabankExercises/APL/AreaPerLipidAndThicknessExamples.ipynb")
+setenv("_COURSE_GIT_REPO", "https://github.com/NMRLipids/DatabankExercises.git")
+setenv("_COURSE_GIT_REF", "")
+setenv("_COURSE_NOTEBOOK_TYPE", "notebook")
+
+-- Extra course material
+setenv("_COURSE_SRC", "/scratch/project_xxxxx/Databank")
